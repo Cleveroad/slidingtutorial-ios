941b880b57d34f1215c49574d93facc03e516581
Danil <voitenko.cr@gmail.com>
2016-11-01 22:03:08 +0200

    replace assets to @2 (universal);

---
+++ Example/SlidingTutorial/PRLTestViewController.m
@@ -0,0 +1,37 @@
+//
+//  PRLTestViewController.m
+//  SlidingTutorial
+//
+//  Created by Danil on 11/1/16.
+//  Copyright © 2016 Vladislav. All rights reserved.
+//
+
+#import "PRLTestViewController.h"
+
+@interface PRLTestViewController ()
+
+@end
+
+@implementation PRLTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
